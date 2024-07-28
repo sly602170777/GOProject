4bb4d4d220cd5a3afeb068c2eeb749358c6f1713
sly <sly6021070777@gmail.com>
2024-07-28 23:05:42 +0900

    学习文档上传

---
+++ note/AIChat.md
@@ -0,0 +1,3 @@
+# Prompts(提示词)
+
+`https://waytoagi.feishu.cn/wiki/Q5mXww4rriujFFkFQOzc8uIsnah?table=tbljThjDXX4mNdBs&view=vewo2g2ktO`
---
+++ src/20240722/PolymorphismDemo/Polymorphism04/main/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+func test(items ...interface{}) {
+	for index, v := range items {
+		index++
+		switch v.(type) {
+		case bool:
+			fmt.Printf("第%v个参数是 bool 类型，值是%v\n", index, v)
+		case float32, float64:
+			fmt.Printf("第%v个参数是 float 类型，值是%v\n", index, v)
+		case int, int8, int16, int32, int64:
+			fmt.Printf("第%v个参数是 整数 类型，值是%v\n", index, v)
+		case string:
+			fmt.Printf("第%v个参数是 字符串 类型，值是%v\n", index, v)
+		default:
+			fmt.Printf("第%v个参数是 未知类型%v\n", index, v)
+		}
+
+	}
+}
+
+type Student struct {
+}
+
+func main() {
+	fmt.Println()
+	var n1 float32 = 1.1
+	var n2 float64 = 2.3
+	var n3 int32 = 30
+	var name string = "tom"
+	address := "北京"
+	n4 := 300
+
+	stu1 := Student{}
+	stu2 := &Student{}
+	test(n1, n2, n3, n4, name, address, stu1, stu2)
+
+	//冒泡排序
+	fmt.Println("冒泡排序")
+
+	var arr = []int{1, 5, 8, 4, 2, 9, 3, 7, 6}
+	fmt.Println("排序前：", arr)
+	for i := 0; i < len(arr)-1; i++ { //控制比较轮数
+		for j := 0; j < len(arr)-1-i; j++ { //控制每轮比较次数
+			if arr[j] > arr[j+1] { //如果前一个数大于后一个数，则交换位置
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	fmt.Println("排序后：", arr)
+
+	//生成一个多表查询的SQL
+	fmt.Println("生成一个多表查询的SQL")
+}
